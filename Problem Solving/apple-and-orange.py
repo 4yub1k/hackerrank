c7150bd2bc56fc63053bc45e5fde56606204478f
Salah Ud Din <45902447+4yub1k@users.noreply.github.com>
2022-09-04 19:33:54 +0500

    Apple And Orange

--- Problem Solving/apple-and-orange.py
+++ Problem Solving/apple-and-orange.py
@@ -5,36 +5,9 @@ Itertools : https://docs.python.org/3/library/itertools.html#itertools.zip_longe
 """
 
 
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'countApplesAndOranges' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER s
-#  2. INTEGER t
-#  3. INTEGER a
-#  4. INTEGER b
-#  5. INTEGER_ARRAY apples
-#  6. INTEGER_ARRAY oranges
-#
-
-#!/bin/python3
 # zip_longest will by default fill unequal list value with None, and you can custom <fill fillvalue=0>
 # it is similar to zip but can handle [1, 2, 3], [4, 5] = [1,4], [2, 5], [3, None]
 from itertools import zip_longest
-
-import math
-import os
-import random
-import re
-import sys
 
 #
 # Complete the 'countApplesAndOranges' function below.
@@ -69,23 +42,19 @@ if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
 
     s = int(first_multiple_input[0])
-
     t = int(first_multiple_input[1])
 
     second_multiple_input = input().rstrip().split()
 
     a = int(second_multiple_input[0])
-
     b = int(second_multiple_input[1])
 
     third_multiple_input = input().rstrip().split()
 
     m = int(third_multiple_input[0])
-
     n = int(third_multiple_input[1])
 
     apples = list(map(int, input().rstrip().split()))
-
     oranges = list(map(int, input().rstrip().split()))
 
     countApplesAndOranges(s, t, a, b, apples, oranges)
@@ -113,23 +82,19 @@ if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
 
     s = int(first_multiple_input[0])
-
     t = int(first_multiple_input[1])
 
     second_multiple_input = input().rstrip().split()
 
     a = int(second_multiple_input[0])
-
     b = int(second_multiple_input[1])
 
     third_multiple_input = input().rstrip().split()
 
     m = int(third_multiple_input[0])
-
     n = int(third_multiple_input[1])
 
     apples = list(map(int, input().rstrip().split()))
-
     oranges = list(map(int, input().rstrip().split()))
 
     countApplesAndOranges(s, t, a, b, apples, oranges)
